79820e2142b1c9f5e01d9f081ca7d6ecd21641fe
Tobiloba48 <tobisalam48@gmail.com>
2024-10-22 14:22:29 +0300

    firstmessage

---
+++ login.js
@@ -0,0 +1,17 @@
+const login = document.getElementById("login");
+
+const backgroundImages = [
+  'url("/Assets/Images/nysc1.jpg")',
+  'url("/Assets/Images/nysc2.jpg")',
+];
+
+let currentIndex = 0;
+function changeBackgroundImage() {
+  login.style.backgroundImage = backgroundImages[currentIndex];
+  currentIndex = (currentIndex + 1) % backgroundImages.length;
+}
+
+setInterval(changeBackgroundImage,2000)
+
+let form = document.querySelector('formcontainer')
+form.style.backgroundcolor.black
